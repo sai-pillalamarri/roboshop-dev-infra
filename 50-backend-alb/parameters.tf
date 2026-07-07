342c38ee9c0165b809e815ff509aa4edbbbf567c
sainagaraju <nagarajusai91@gmail.com>
2026-07-07 16:40:29 +0530

    update

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "backend_alb_arn" {
   name      = "/${var.project}/${var.environment}/backend_alb_arn"
   type      = "String"
-  value     = aws_alb.backend_alb.arn
+  value     = aws_alb_listener.backend_alb.arn
   overwrite = true
 
 }
