8d57cb0f13117a620821428d6d60735ef41bd54e
sainagaraju <nagarajusai91@gmail.com>
2026-07-07 05:30:25 +0530

    region typo fixed

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "backend_alb_arn" {
   name      = "/${var.project}/${var.environment}/backend_alb_arn"
-  type      = string
+  type      = "String"
   value     = aws_alb.backend_alb.arn
   overwrite = true
 
--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -16,6 +16,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1a"
+  region = "us-east-1"
 
 }
